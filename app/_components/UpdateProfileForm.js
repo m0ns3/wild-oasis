5090866fba06abf08a7e5e5daeebbde3331717f5
Monserrat <monserrat.diaz@mitrol.net>
2025-04-13 08:08:55 +0300

    Manual Cache Revalidation

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -2,18 +2,24 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import { updateGuest } from "@/app/_lib/actions";
 
-export default function UpdateProfileForm({ children }) {
-  const countryFlag = "/pt.jpg";
-
+export default function UpdateProfileForm({ children, guest }) {
   const [count, setCount] = useState(0);
 
+  const { fullName, email, nationality, nationalID, countryFlag } = guest;
+
   return (
-    <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+    <form
+      action={updateGuest}
+      className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
+          name="fullName"
           disabled
+          defaultValue={fullName}
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -21,7 +27,9 @@ export default function UpdateProfileForm({ children }) {
       <div className="space-y-2">
         <label>Email address</label>
         <input
+          name="email"
           disabled
+          defaultValue={email}
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -29,9 +37,8 @@ export default function UpdateProfileForm({ children }) {
       <div className="space-y-2">
         <div className="flex items-center justify-between">
           <label htmlFor="nationality">Where are you from?</label>
-          <Image
-            fill
-            src={countryFlag}
+          <img
+            src={countryFlag || ""}
             alt="Country flag"
             className="object-cover h-5 rounded-sm"
           />
@@ -43,6 +50,7 @@ export default function UpdateProfileForm({ children }) {
       <div className="space-y-2">
         <label htmlFor="nationalID">National ID number</label>
         <input
+          defaultValue={nationalID}
           name="nationalID"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
